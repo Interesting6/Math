e7ff39008764fac410e118094e17d803dc546003
Nianyi Wang <WangNianyi2001@gmail.com>
2018-01-26 18:00:48 +0800

    Generalized for other types

--- Math.js
+++ Math.js
@@ -1,19 +1,17 @@
 {
 	window.math = {};
 	let Tensor = math.Tensor = js.Class(function Tensor(value) {
-			if(+value === value) {
-				this.value = +value;
-				this.dimension = 0;
-				this.size = [];
-			} else if(value instanceof Array) {    // value is iteratable, should've use that Symbol or something
+			if(value instanceof Array) {    // value is iteratable, should've use that Symbol or something
 				this.value = value.map(v => new Tensor(v));
 				this.dimension = this.value[0].dimension + 1;
 				this.size = [value.length].concat(this.value[0].size);
 			} else if(js.IsInstanceOf(Tensor)(value)) {
 				this.value = new Tensor(value.Raw()).value;
 				this.size = value.size.slice();
-			} else{
-				throw new TypeError('Invalid tensor value');
+			} else {
+				this.value = value;
+				this.dimension = 0;
+				this.size = [];
 			}
 		}
 	)
---
+++ index.js
@@ -0,0 +1,7 @@
+let vector = math.Tensor([
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8]
+]);
+console.log(vector);
+console.log(new math.Tensor(vector));
