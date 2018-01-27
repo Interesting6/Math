b8f86b021217557bc4530858b0e2d2d85b9930d4
Nianyi Wang <WangNianyi2001@gmail.com>
2018-01-27 13:18:04 +0800

    Update test cases

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let vector = math.Tensor([
+let matrix0 = math.Tensor([
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8]
 ]);
-console.log(vector);
-console.log(new math.Tensor(vector));
+let vector0 = math.Tensor([0, 1, 2]);
+let vector1 = math.Tensor([1, 2, 3]);
+console.log(matrix0.Dot(vector0).Raw());
+console.table(vector0.Product(vector1).Raw());
