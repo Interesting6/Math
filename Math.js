2f65589af896267169f6316576e5f15395f091d8
Nianyi Wang <WangNianyi2001@gmail.com>
2018-01-27 16:01:24 +0800

    New: math.Number

--- Math.js
+++ Math.js
@@ -1,37 +1,40 @@
 {
 	window.math = {};
+	let Number = math.Number = js.Class(function Number() {
+	
+	})({})({});
 	let Tensor = math.Tensor = js.Class(function Tensor(value) {
 		if(typeof value[Symbol.iterator] !== 'undefined') {
 			this.value = [];
 			for(let row of value)
 				this.value.push(new Tensor(row));
-			this.dimension = this.value[0].dimension + 1;
+			this.degree = this.value[0].degree + 1;
 			this.size = [value.length].concat(this.value[0].size);
 		} else if(js.IsInstanceOf(Tensor)(value)) {
 			this.value = new Tensor(value.Raw()).value;
-			this.dimension = value.dimension;
+			this.degree = value.degree;
 			this.size = value.size.slice();
 		} else {    // default case, e.g. numbers, complex numbers (not finished yet)
 			this.value = value;
-			this.dimension = 0;
+			this.degree = 0;
 			this.size = [];
 		}
 		// validate size
-		if(this.dimension) {
+		if(this.degree) {
 			let standard = this.value[0].value.length;
 			if(this.value.some(row => row.value.length !== standard))
 				throw new RangeError('The tensor is not in a uniform size');
 		}
 	})({})({
 		Raw() {
-			return this.dimension === 0 ? this.value : this.value.map(value => value.Raw());
+			return this.degree === 0 ? this.value : this.value.map(value => value.Raw());
 		},
 		Product(operatee) {
 			if(!(operatee instanceof Tensor))
 				operatee = new Tensor(operatee);
 			return new Tensor(
-				operatee.dimension === 0 ?
-					this.dimension === 0 ?
+				operatee.degree === 0 ?
+					this.degree === 0 ?
 						this.value * operatee.value :
 						this.value.map(value => value.Product(operatee)) :
 					operatee.value.map(sub => this.Product(sub))
@@ -40,16 +43,16 @@
 		Plus(operatee) {
 			if(!(operatee instanceof Tensor))
 				operatee = new Tensor(operatee);
-			if(operatee.dimension !== this.dimension || !js.Equal(operatee.size)(this.size))
+			if(operatee.degree !== this.degree || !js.Equal(operatee.size)(this.size))
 				throw new RangeError('Cannot plus two tensors in different sizes');
 			return new Tensor(
-				this.dimension === 0 ?
+				this.degree === 0 ?
 					this.value + operatee.value :
 					this.value.map((v, i) => v.Plus(operatee.value[i]))
 			);
 		},
 		Get(indexes) {
-			return indexes.length ? new Tensor(this.dimension ? this.value[indexes[0]].Get(indexes.slice(1)) : this.value) : this;
+			return indexes.length ? new Tensor(this.degree ? this.value[indexes[0]].Get(indexes.slice(1)) : this.value) : this;
 		},
 		Set(indexes, operatee) {
 			if(!(operatee instanceof Tensor))
@@ -59,8 +62,8 @@
 		Dot(operatee) {
 			if(!(operatee instanceof Tensor))
 				operatee = new Tensor(operatee);
-			return new Tensor(!operatee.dimension ?
-				!this.dimension ?
+			return new Tensor(!operatee.degree ?
+				!this.degree ?
 					operatee.value * this.value :
 					this.value.map(value => value.Dot(operatee)).reduce((a, b) => a.Plus(b), new Tensor(0)) :
 				operatee.value.map(sub => this.Dot(sub)).reduce((a, b) => a.Plus(b), new Tensor(0))
