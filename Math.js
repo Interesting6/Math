77a5dca0b794896224a3237ff0d5112ebd26f39b
Nianyi Wang <WangNianyi2001@gmail.com>
2018-01-27 20:03:15 +0800

    New: math.Number argument & modulus getter & setter

--- Math.js
+++ Math.js
@@ -1,23 +1,45 @@
 {
 	window.math = {};
-	let Number = math.Number = js.Class(function Number(real, imaginary) {
+	math.Number = js.Class(function Number(real, imaginary) {
 		if(arguments.length === 1) {
-			if(real instanceof window.Number)
-				return new Number(real, 0);
-			else if(real instanceof Number)
-				return new Number(real.real, real.imaginary);
-			else
+			if(+real === real) {
+				[this.real, this.imaginary] = [real, 0];
+			} else if(real instanceof math.Number) {
+				this.real = real.real;
+				this.imaginary = real.imaginary;
+			} else
 				throw new TypeError('Unresolvable input type');
 		} else {
 			[real, imaginary] = [+real, +imaginary];
 			if(isNaN(real) || isNaN(imaginary))
-				throw new TypeError('The components of a number must not be NaN');
+				throw new TypeError('The components of a number0 must not be NaN');
 			[this.real, this.imaginary] = [real, imaginary];
 		}
 	})({})({
-		// prototypes
+		get modulus() {
+			return this.real * this.real + this.imaginary * this.imaginary;
+		},
+		set modulus(value) {
+			if(this.real === this.imaginary === 0)
+				this.real = value;
+			else {
+				let ratio = value / this.modulus;
+				this.real *= ratio;
+				this.imaginary *= ratio;
+			}
+		},
+		get argument() {
+			return Math.atan2(this.imaginary, this.real);
+		},
+		set argument(value) {
+			let sin = Math.sin(value), cos = Math.cos(value);
+			[this.real, this.imaginary] = [
+				this.real * cos - this.imaginary * sin,
+				this.real * sin + this.imaginary * cos
+			];
+		}
 	});
-	let Tensor = math.Tensor = js.Class(function Tensor(value) {
+	math.Tensor = js.Class(function Tensor(value) {
 		if(typeof value[Symbol.iterator] !== 'undefined') {
 			this.value = [];
 			for(let row of value)
@@ -44,9 +66,9 @@
 			return this.degree === 0 ? this.value : this.value.map(value => value.Raw());
 		},
 		Product(operatee) {
-			if(!(operatee instanceof Tensor))
-				operatee = new Tensor(operatee);
-			return new Tensor(
+			if(!(operatee instanceof math.Tensor))
+				operatee = new math.Tensor(operatee);
+			return new math.Tensor(
 				operatee.degree === 0 ?
 					this.degree === 0 ?
 						this.value * operatee.value :
@@ -55,32 +77,32 @@
 			);
 		},
 		Plus(operatee) {
-			if(!(operatee instanceof Tensor))
-				operatee = new Tensor(operatee);
+			if(!(operatee instanceof math.Tensor))
+				operatee = new math.Tensor(operatee);
 			if(operatee.degree !== this.degree || !js.Equal(operatee.size)(this.size))
 				throw new RangeError('Cannot plus two tensors in different sizes');
-			return new Tensor(
+			return new math.Tensor(
 				this.degree === 0 ?
 					this.value + operatee.value :
 					this.value.map((v, i) => v.Plus(operatee.value[i]))
 			);
 		},
 		Get(indexes) {
-			return indexes.length ? new Tensor(this.degree ? this.value[indexes[0]].Get(indexes.slice(1)) : this.value) : this;
+			return indexes.length ? new math.Tensor(this.degree ? this.value[indexes[0]].Get(indexes.slice(1)) : this.value) : this;
 		},
 		Set(indexes, operatee) {
-			if(!(operatee instanceof Tensor))
-				operatee = new Tensor(operatee);
+			if(!(operatee instanceof math.Tensor))
+				operatee = new math.Tensor(operatee);
 			indexes.length ? this.value[indexes[0]].Set(indexes.slice(1), operatee) : this.value = operatee.value;
 		},
 		Dot(operatee) {
-			if(!(operatee instanceof Tensor))
-				operatee = new Tensor(operatee);
-			return new Tensor(!operatee.degree ?
+			if(!(operatee instanceof math.Tensor))
+				operatee = new math.Tensor(operatee);
+			return new math.Tensor(!operatee.degree ?
 				!this.degree ?
 					operatee.value * this.value :
-					this.value.map(value => value.Dot(operatee)).reduce((a, b) => a.Plus(b), new Tensor(0)) :
-				operatee.value.map(sub => this.Dot(sub)).reduce((a, b) => a.Plus(b), new Tensor(0))
+					this.value.map(value => value.Dot(operatee)).reduce((a, b) => a.Plus(b), new math.Tensor(0)) :
+				operatee.value.map(sub => this.Dot(sub)).reduce((a, b) => a.Plus(b), new math.Tensor(0))
 			)
 		}
 	});
--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-let matrix0 = math.Tensor([
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8]
-]);
-let vector0 = math.Tensor([0, 1, 2]);
-let vector1 = math.Tensor([1, 2, 3]);
-
-console.table(vector0.Product(vector1).Raw());
-console.log(vector0.Dot(vector1).Raw());
+let number0 = new math.Number(0, 1);
+number0.modulus = 2;
+number0.argument = Math.PI / 2;
+console.log(number0);
